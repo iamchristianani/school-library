536603fd103a81cee8ccaa05088069a2094792a5
Christian Ani <iamchristianani@gmail.com>
2023-03-06 20:47:31 +0100

    Initialise parent_permission

--- person.rb
+++ person.rb
@@ -1,8 +1,9 @@
 class Person
-  def initialize(age, name = 'Unknown', _parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   attr_reader :id
