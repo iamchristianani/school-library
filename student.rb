37416383d4d8594aa57486824c54ce0cb096a60b
Christian Ani <iamchristianani@gmail.com>
2023-03-08 17:25:40 +0100

    Update student class to add classroom method

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
