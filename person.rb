ff0b51f582edcfdcddd4cb8ad11fd29af1acd41b
Christian Ani <iamchristianani@gmail.com>
2023-03-08 17:24:24 +0100

    Update person class to add rentals

--- person.rb
+++ person.rb
@@ -7,9 +7,10 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def of_age?
