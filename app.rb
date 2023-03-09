55ecd038d7839f043c8707451d25d74f59279b9c
Christian Ani <iamchristianani@gmail.com>
2023-03-09 18:31:57 +0100

    Fix Linter errors

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-require "./person"
-require "./rental"
-require "./book"
-require "./classroom"
-require "./student"
-require "./teacher"
-require "./nameable"
+require './person'
+require './rental'
+require './book'
+require './classroom'
+require './student'
+require './teacher'
+require './nameable'
 
 class App
   def initialize
@@ -14,27 +14,27 @@ class App
   end
 
   def list_books
-    if @books.length == 0
-      "There are no books"
+    if @books.empty?
+      'There are no books'
     else
-      @books.each do |eachBook| 
-        puts "Title: \"#{eachBook.title}\", Author: \"#{eachBook.author}\""
+      @books.each do |each_book|
+        puts "Title: \"#{each_book.title}\", Author: \"#{each_book.author}\""
       end
     end
   end
 
   def list_people
-    if @people.length == 0
-      "There are no people"
+    if @people.empty?
+      'There are no people'
     else
-      @people.each do |eachPerson| 
-        puts "Name: #{eachPerson.name}, ID: #{eachPerson.id}, Age: #{eachPerson.age}"
+      @people.each do |each_person|
+        puts "Name: #{each_person.name}, ID: #{each_person.id}, Age: #{each_person.age}"
       end
     end
   end
 
   def create_person
-    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     number = gets.chomp.to_i
     if number == 1
       create_student
@@ -44,84 +44,83 @@ class App
   end
 
   def create_student
-    print "Age: "
+    print 'Age: '
     age = gets.chomp.to_i
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
-    print "Has parent permission? [Y/N]: "
+    print 'Has parent permission? [Y/N]: '
     parent_permission = gets.chomp
 
     each_student = Student.new(age, name, parent_permission)
     @people << each_student
-    puts "Person created successfully"
+    puts 'Person created successfully'
   end
 
   def create_teacher
-    print "Age: "
+    print 'Age: '
     age = gets.chomp.to_i
-    print "Name: "
+    print 'Name: '
     name = gets.chomp
-    print "Specialization: "
+    print 'Specialization: '
     specialization = gets.chomp
 
     each_teacher = Teacher.new(age, specialization, name)
     @people << each_teacher
-    puts "Person created successfully"
+    puts 'Person created successfully'
   end
 
   def create_book
-    print "Title: "
+    print 'Title: '
     title = gets.chomp
-    print "Author: "
+    print 'Author: '
     author = gets.chomp
 
     each_book = Book.new(title, author)
     @books << each_book
-    puts "Book created successfully"
+    puts 'Book created successfully'
   end
 
   def create_rental
     if @books.empty?
-      puts "Book array is empty"
+      puts 'Book array is empty'
     elsif @people.empty?
-      puts "Person array is empty"
+      puts 'Person array is empty'
     else
-      puts "Select a book from the following list by number"
-      @books.each_with_index do |book, index| 
+      puts 'Select a book from the following list by number'
+      @books.each_with_index do |book, index|
         puts "#{index.to_i}) Title: \"#{book.title}\" Author: #{book.author}"
       end
       rental_book = gets.chomp.to_i
 
-      puts "Select a person from the following list by number (not id)"
-      @people.each_with_index do |person, index| 
+      puts 'Select a person from the following list by number (not id)'
+      @people.each_with_index do |person, index|
         puts "#{index.to_i}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
       rental_person = gets.chomp.to_i
 
-      print "Date: "
+      print 'Date: '
       date = gets.chomp
 
       rental = Rental.new(date, @people[rental_person], @books[rental_book])
 
       @rentals << rental
-      puts "Rental created successfully"
+      puts 'Rental created successfully'
     end
   end
 
   def list_rental_of_person
     if @rentals.empty?
-      "Rental is empty"
+      'Rental is empty'
     else
-      print "ID of person: "
+      print 'ID of person: '
       person_id = gets.chomp.to_i
-  
+
       @rentals.each do |rental|
         if rental.person.id == person_id
-          puts "Rentals: "
+          puts 'Rentals: '
           puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
         end
       end
     end
   end
-
 end
--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ end
 
 class Main
   puts "Welcome to school library app!\n\n"
-  def menu
+  def self.menu
     puts
     puts 'Please choose an option by entering a number'
     @list = {
@@ -21,31 +21,31 @@ class Main
     @list.each do |id, option|
       puts "#{id}: #{option}"
     end
-    num = gets.chomp.to_i
+    gets.chomp.to_i
   end
-  def start
-    res = App.new
-    loop do
-      case menu
-      when 1
-        res.list_books
-      when 2
-        res.list_people
-      when 3
-        res.create_person
-      when 4
-        res.create_book
-      when 5
-        res.create_rental
-      when 6
-        res.list_rental_of_person
-      when 7
-        puts 'Thank you for using this app!'
-        exit
-      else
-        puts 'Please choose a number between 1 and 7'
-      end
+
+  res = App.new
+  loop do
+    case menu
+    when 1
+      res.list_books
+    when 2
+      res.list_people
+    when 3
+      res.create_person
+    when 4
+      res.create_book
+    when 5
+      res.create_rental
+    when 6
+      res.list_rental_of_person
+    when 7
+      puts 'Thank you for using this app!'
+      exit
+    else
+      puts 'Please choose a number between 1 and 7'
     end
   end
 end
-main.start
+
+main
